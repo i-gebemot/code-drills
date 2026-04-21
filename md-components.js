583bc66854875585354c6d7734c6b0bfeea52f9a
gosha-smeh <gosha.smeh@gmail.com>
2026-04-21 15:31:13 +0300

    0.18.12

--- md-components.js
+++ md-components.js
@@ -4376,7 +4376,7 @@
     Class;
     return Class;
   }
-  const VERSION = "0.18.11";
+  const VERSION = "0.18.12";
   const PUBLIC_VERSION = "5";
   if (typeof window !== "undefined") {
     ((window.__svelte ??= {}).v ??= /* @__PURE__ */ new Set()).add(PUBLIC_VERSION);
