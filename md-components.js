1a22eda17d2cf7c68a8ff90840a9569e37ff112b
gosha-smeh <gosha.smeh@gmail.com>
2026-06-16 04:34:00 +0300

    0.29.1

--- md-components.js
+++ md-components.js
@@ -4492,7 +4492,7 @@
     Class;
     return Class;
   }
-  const VERSION = "0.29.0";
+  const VERSION = "0.29.1";
   const PUBLIC_VERSION = "5";
   if (typeof window !== "undefined") {
     ((window.__svelte ??= {}).v ??= /* @__PURE__ */ new Set()).add(PUBLIC_VERSION);
@@ -13567,7 +13567,7 @@
     };
     var div = root$b();
     let classes;
-    set_style(div, "", {}, { "--width": "600" });
+    set_style(div, "", {}, { "--width": "780" });
     var node = child(div);
     html(node, content);
     reset(div);
